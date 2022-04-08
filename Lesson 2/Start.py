f3f31b525c568ce932cf488ea992b2bf50b6a79e
unknown <michael.gydin@gmail.com>
2022-04-08 11:45:39 +0300

    Lesson2

--- Lesson 2/Start.py
+++ Lesson 2/Start.py
@@ -1,9 +1,6 @@
 # Перший код - як вивести текст
 
-from locale import ABDAY_2
-
-
-print("Hello World!")
+#print("Hello World!")
 
 # Другий код - змінна
 
@@ -11,74 +8,93 @@ print("Hello World!")
 #! Ім'я змінної не може починатися з числа.
 #! Регістр для змінних важливий, тобто є різниця між var і Var.
 
-first = 23
-print(first)
+#first = 23
+#print(first)
 
 #-------------------------------------------------------------------
-second = "Number"
-print(second)
+#second = "Number"
+#print(second)
 
 #-------------------------------------------------------------------
-print(first, "is a", second)
+#print(first, "is a", second)
 
 # Третій код - як проводити операції над числами.
 
 #! Для того щоб поєднати дві змінні в одну (одного типу) використовується +.
 
-a_1 = 10 + 5
-b_1 = a_1 + 20
-c_1 = (a_1 + b_1) * 2
-print(a_1, b_1, c_1)
+#a_1 = 10 + 5
+#b_1 = a_1 + 20
+#c_1 = (a_1 + b_1) * 2
+#print(a_1, b_1, c_1)
 
 # Чевертий код - робота з рядками
 
 #! Під час створення рядка, враховується тільки перші лапки.
 
-first_string = "Hello World!"
-second_string = 'Hello World!'
-third_string = """Hello World!"""
+#first_string = "Hello World!"
+#second_string = 'Hello World!'
+#third_string = """Hello World!"""
 
 #-------------------------------------------------------------------
 
-name = input("Ваше ім'я? ")
-greeting = "Привіт, " + name
-print(greeting)
+#name = input("Ваше ім'я? ")
+#greeting = "Привіт, " + name
+#print(greeting)
 
 #-------------------------------------------------------------------
 
 # Для того щоб вивести довжину слова використоємо len(змінна)
-string = input("Напишіть любий текст: ")
-string_length = len(string)
-print(string_length)
+#string = input("Напишіть любий текст: ")
+#string_length = len(string)
+#print(string_length)
 
 # Пятий код - типи данних, перетворення типів.
 
 #! Для того щоб конвертувати любе значення любе значення в текст пишемо str(змінна).
-#! Для того щоб конвертувати любе значення любе значення в текст пишемо int(змінна).
+#! Для того щоб конвертувати любе значення любе значення в число пишемо int(змінна).
 
-result_1 = 10 + 15
-text_1 = "Результат = " + str(result_1)
-print(text_1)
-
-#-------------------------------------------------------------------
-
-result_2 = "10" + "15"
-text_2 = "Результат = " + (result_2)
-print(text_2)
+#result_1 = 10 + 15
+#text_1 = "Результат = " + str(result_1)
+#print(text_1)
 
 #-------------------------------------------------------------------
 
-a_2 = input("Перше число: ")
-b_2 = input("Друге число: ")
-result_3 = int(a_2) + int(b_2)
-text_3 = "Результат = " + str(result_3)
-print(text_3)
+#result_2 = "10" + "15"
+#text_2 = "Результат = " + (result_2)
+#print(text_2)
+
+#-------------------------------------------------------------------
+
+#a_2 = input("Перше число: ")
+#b_2 = input("Друге число: ")
+#result_3 = int(a_2) + int(b_2)
+#text_3 = "Результат = " + str(result_3)
+#print(text_3)
 
 # Шостий код - логічна перевірка if + elif + else
 
 first_number = input("Перше число: ")
-if int(first_number) == 0: 
-    print("Ділення на 0 не можливе.")
-else: 
-    print("Ділення можливо.")
+first_simbol = input("Перший cимвол (+ або -): ")
+second_number = input("Друге число: ")
+second_simbol = input("Другий символ (+ або -): ")
+third_number = input("Третє число: ")
 
+if first_simbol == "+":
+    int(first_number) + int(second_number)
+else: 
+    print("Це не + або -!")
+
+if first_simbol == "-":
+    int(first_number) - int(second_number)
+else: 
+    print("Це не + або -!")
+
+if second_simbol == "+":
+    int(first_number) + int(first_simbol) + int(second_number) + int(third_number)
+else: 
+    print("Це не + або -!")
+
+if second_simbol == "-":
+    int(first_number) + first_simbol + int(second_number) - int(third_number)
+else: 
+    print("Це не + або -!")
