ffff774cb90d7cbb9ba9d8838d683d99bce9457f
unknown <michael.gydin@gmail.com>
2022-04-21 17:52:51 +0300

    Lesson7

---
+++ Lesson 7/Less7.py
@@ -0,0 +1,64 @@
+import random
+class Human:
+
+    def __init__(self, name = input("Enter your name: "), job = None, car = None, home = None):
+        self.name = name
+        self.money = 100
+        self.gladness = 100
+        self.satieti = 100
+        self.job = job
+        self.home = home
+        self.car = car
+
+    def get_job(self):
+        pass
+
+    def get_home(self):
+        pass
+
+    def get_car(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def work(self):
+        pass
+
+    def shopping(self, manage):
+        pass
+
+    def chill(self):
+        pass
+
+    def clean_home(self):
+        pass
+
+    def days_index(self, day):
+        pass
+
+    def is_alive(self):
+        pass
+
+    def live(self, day):
+        pass
+
+class Job:
+
+    def __init__(self):
+        self.salary = 500
+        self.gladness_less = 
+        self.job = 
+
+class House:
+
+    def __init__(self):
+        self.mess = 0
+        self.food = 50
+
+class Auto:
+
+    def __init__(self,brand):
+        self.brand = brand
+        self.fuel = 100
+
