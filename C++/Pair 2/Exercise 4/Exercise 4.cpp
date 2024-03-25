54c268ef8248e54efbf30ec288246f44a9070782
The Lone_ <102746222+MichaelGydin@users.noreply.github.com>
2024-03-25 17:30:53 +0200

    Pair 2 & 4

---
+++ C++/Pair 2/Exercise 4/Exercise 4.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{   /* // Розділ 1
+    // Завдання 1
+    int a, sum = 0; cout << "Enter number: "; cin >> a;
+
+    for (int i = a; i <= 500; i++) {
+        sum += i;
+    } cout << "Sum of integers from " << a << " to 500: " << sum << endl;
+
+    // Завдання 2
+    int x, y;
+    cout << "Enter x: "; cin >> x;
+    cout << "Enter y: "; cin >> y;
+
+    int result = 1;
+    for (int i = 0; i < y; i++) {
+        result *= x;
+    } cout << x << " raised to the power of " << y << " is: " << result << endl;
+
+    // Завдання 3
+    int sum = 0, count = 0, avg = sum / count;
+
+    for (int i = 1; i <= 1000; i++) {
+        sum += i;
+        count++;
+    } cout << "Average 1 to 1000: " << avg << endl;
+
+    // Завдання 4
+    int a; cout << "Enter number (1 < a < 20): "; cin >> a;
+
+    if (a < 1 || a > 20) {
+        cout << "Invalid input." << endl;
+        return 1;
+    }
+
+    int product = 1;
+    for (int i = a; i <= 20; i++) {
+        product *= i;
+    } cout << "Product of numbers from " << a << " to 20: " << product << endl;
+
+    // Завдання 5
+    int k; cout << "Enter number: "; cin >> k;
+
+    cout << "Multiplication table for " << k << ":" << endl;
+    for (int i = 2; i <= 10; i++) {
+        cout << k << " x " << i << " = " << k * i << endl;
+    } */
+
+    /* // Розділ 2
+    // Завдання 1
+    int count = 0;
+    for (int i = 100; i < 1000; i++) {
+        int hundreds = i / 100;
+        int tens = (i / 10) % 10;
+        int units = i % 10;
+        if (hundreds == tens || hundreds == units || tens == units) {
+            count++;
+        }
+    } cout << "Number of integers with two identical digits: " << count << endl;
+
+    // Завдання 2
+    int count = 0;
+    for (int i = 100; i < 1000; i++) {
+        int hundreds = i / 100;
+        int tens = (i / 10) % 10;
+        int units = i % 10;
+        if (hundreds != tens && hundreds != units && tens != units) {
+            count++;
+        }
+    } cout << "Number of integers with all different digits: " << count << endl;
+
+    // Завдання 3
+    int number, result = 0, multiplier = 1; cout << "Enter an integer: "; cin >> number;
+
+    while (number != 0) {
+        int digit = number % 10;
+        if (digit != 3 && digit != 6) {
+            result += digit * multiplier;
+            multiplier *= 10;
+        }
+        number /= 10;
+    } cout << "Number after removing digits 3 and 6: " << result << endl;
+
+    // Завдання 4
+    int a; cout << "Enter A: "; cin >> a;
+
+    cout << "Integers B for which A divides B^2 but not B^3: ";
+    for (int b = 1; b <= a; b++) {
+        if (a % (b * b) == 0 && a % (b * b * b) != 0) {
+            cout << b << " ";
+        }
+    } cout << endl;
+
+    // Завдання 5
+    int a, sum = 0, temp = a; cout << "Enter an integer A: "; cin >> a;
+
+    while (temp != 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+
+    if (sum * sum * sum == a * a) {
+        cout << "The cube of the sum of the digits is equal to A*A." << endl;
+    }
+    else {
+        cout << "The cube of the sum of the digits is not equal to A*A." << endl;
+    }
+
+    // Завдання 6
+    int number; cout << "Enter an integer: "; cin >> number;
+
+    cout << "Integers that " << number << " is divisible by without remainder: ";
+    for (int i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            cout << i << " ";
+        }
+    } cout << endl;
+
+    // Завдання 7
+    int number1, number2;
+    cout << "Enter the first integer: "; cin >> number1;
+    cout << "Enter the second integer: "; cin >> number2;
+
+    int maxNumber = (number1 > number2) ? number1 : number2;
+
+    cout << "Integers that both " << number1 << " and " << number2 << " are divisible by without remainder: ";
+    for (int i = 1; i <= maxNumber; i++) {
+        if (number1 % i == 0 && number2 % i == 0) {
+            cout << i << " ";
+        }
+    } cout << endl << endl; */
+}
---
+++ C++/Pair 4/Exercise 7/Exercise 7.cpp
@@ -0,0 +1,144 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Розділ 1
+// Завдання 1
+template<typename B>
+B power(B base, int exponent) {
+    B result = 1.0;
+    int positExp = exponent;
+    if (exponent < 0) {
+        positExp = -exponent;
+    }
+    for (int i = 0; i < positExp; ++i) {
+        result *= base;
+    }
+    if (exponent < 0) {
+        result = 1.0 / result;
+    }
+    return result;
+}
+
+// Завдання 2
+int sumRange(int a, int b) {
+    int sum = 0, start = (a < b) ? a : b, end = (a < b) ? b : a;
+    for (int i = start + 1; i < end; ++i) {
+        sum += i;
+    }
+    return sum;
+}
+
+// Завдання 3
+bool isPerfect(int number) {
+    int sum = 0;
+    for (int i = 1; i <= number / 2; ++i) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+    return sum == number;
+}
+
+void findNumbers(int start, int end) {
+    cout << "Perfect numbers [" << start << ", " << end << "]:" << endl;
+    for (int i = start; i <= end; ++i) {
+        if (isPerfect(i)) {
+            cout << i << endl;
+        }
+    }
+}
+
+// Завдання 4
+void Card(int number) {
+    string suits[] = { "Gold", "Diamonds", "Pencil", "Spider" };
+    string ranks[] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Clown", "Queen", "King" };
+
+    int suit = number / 13;
+    int rank = number % 13;
+
+    cout << ranks[rank] << " of " << suits[suit] << endl;
+}
+
+// Завдання 5
+bool isLucky(int number) {
+    int sumFirstHalf = 0;
+    int sumSecondHalf = 0;
+
+    for (int i = 0; i < 3; ++i) {
+        sumFirstHalf += number % 10;
+        number /= 10;
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        sumSecondHalf += number % 10;
+        number /= 10;
+    }
+
+    return sumFirstHalf == sumSecondHalf;
+}
+
+// Розділ 2
+// Завдання 1
+
+// Ще не вчили!!!
+
+// Завдання 2
+
+// Завдання 3
+
+int main() {
+    // Розділ 1
+    // Завдання 1
+    double baseD;
+    int baseI, exponent;
+
+    cout << "Enter base (double): "; cin >> baseD;
+    cout << "Enter exponent: "; cin >> exponent;
+
+    double resultD = power(baseD, exponent);
+    cout << baseD << " ^ " << exponent << " = " << resultD << endl;
+
+    cout << "Enter base (int): "; cin >> baseI;
+
+    int resultInt = power(baseI, exponent);
+    cout << baseI << " ^ " << exponent << " = " << resultInt << endl;
+
+    // Завдання 2
+    cout << "Enter two integers: "; int a, b; cin >> a >> b;
+ 
+    int result = sumRange(a, b); cout << "Sum of numbers between " << a << " and " << b << ": " << result << endl;
+
+    // Завдання 3
+    int start, end;
+
+    cout << "Enter start range: "; cin >> start;
+    cout << "Enter end range: "; cin >> end;
+
+    findNumbers(start, end);
+
+    // Завдання 4
+    cout << "Enter card number (0-51): "; int cardNumber; cin >> cardNumber;
+
+    Card(cardNumber);
+
+    // Завдання 5
+    cout << "Enter six-digit number: "; int number; cin >> number;
+
+    if (isLucky(number)) {
+        cout << "The number is lucky!" << endl;
+    }
+    else {
+        cout << "The number is not lucky." << endl;
+    }
+
+    // Розділ 2
+    // Завдання 1
+
+    // Ще не вчили!!!
+
+    // Завдання 2
+
+    // Завдання 3
+
+}
